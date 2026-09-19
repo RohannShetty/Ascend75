766476e86972d635baa699911f992916d433f72d
Rohan Shetty <shettyrohan2@gmail.com>
2026-09-19 17:37:13 +0530

    fix(learn): add missing core:datastore dependency to feature:learn

--- feature/learn/build.gradle.kts
+++ feature/learn/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:designsystem"))
     implementation(project(":core:database"))
+    implementation(project(":core:datastore"))
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
